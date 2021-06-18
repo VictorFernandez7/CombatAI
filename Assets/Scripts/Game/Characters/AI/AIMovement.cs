37c11b11ebe10b6dbdb9e3013d68d6a6596e9f8e
Victor <victorfernandez95@hotmail.com>
2021-06-19 01:24:44 +0200

    Cleanup

---
+++ Assets/Scripts/Game/Characters/AI/AIMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using System.Collections;
+using UnityEngine;
+
+namespace CombatAI.Game.Characters.AI
+{
+    public class AIMovement : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/Game/Characters/Player/PlayerAttack.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using System.Collections;
+using UnityEngine;
+
+namespace CombatAI.Game.Characters.Player
+{
+    public class PlayerAttack : MonoBehaviour
+    {
+
+    }
+}
