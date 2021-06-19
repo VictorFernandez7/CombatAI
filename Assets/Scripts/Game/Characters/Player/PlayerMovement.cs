c4d86dcde8cef0ccd6e00944db01132adda613f6
Victor <victorfernandez95@hotmail.com>
2021-06-19 04:05:23 +0200

    New movement + fixed jump

--- Assets/Scripts/Game/Characters/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Characters/Player/PlayerMovement.cs
@@ -7,20 +7,10 @@ namespace CombatAI.Game.Characters.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [Title("Horizontal Movement Parameters")]
-        [SerializeField] private float _movementAcceleration = 50f;
-        [SerializeField] private float _maxMoveSpeed = 5f;
-        [SerializeField] private float _groundLinearDrag = 20f;
-
-        [Title("Jump Parameters")]
-        [SerializeField] private float _jumpForce = 10f;
-        [SerializeField] private float _fallMultiplier = 8f;
-        [SerializeField] private float _lowJumpFallMultiplier = 5f;
-        [SerializeField] private float _airLinearDrag = 2.5f;
-        [SerializeField] private float _groundRaycastLength = 0.75f;
-        [SerializeField] private LayerMask _groundLayer;
-
-        public bool changingDirection => (_rigidbody2D.velocity.x > 0f && horizontalDirection < 0f) || (_rigidbody2D.velocity.x < 0f && horizontalDirection > 0f);
+        [Title("Parameters")]
+        [SerializeField] private float _movementSpeed = 5f;
+        [SerializeField] private float _jumpForce = 5f;
+        [SerializeField] private Vector2 _jumpDirection;
 
         public float horizontalDirection
         {
@@ -30,10 +20,25 @@ namespace CombatAI.Game.Characters.Player
                 _horizontalDirection = value;
                 _animator.SetFloat("Speed", Mathf.Abs(value));
                 if (value != 0)
+                {
                     _visuals.localScale = new Vector3(value, _visuals.localScale.y, _visuals.localScale.z);
+                    if (Mathf.Sign(_jumpDirection.x) != value)
+                        _jumpDirection = new Vector2(_jumpDirection.x * -1, _jumpDirection.y);
+                }
             }
         }
 
+        public bool grounded
+        {
+            get => _grounded;
+            set
+            {
+                _grounded = value;
+                _animator.SetBool("Grounded", value);
+            }
+        }
+
+        private bool _canMove;
         private bool _grounded;
         private float _horizontalDirection;
         private Animator _animator;
@@ -51,41 +56,38 @@ namespace CombatAI.Game.Characters.Player
         {
             horizontalDirection = GetInput().x;
 
-            if (Input.GetButtonDown("Jump") && _grounded)
+            if (Input.GetButtonDown("Jump") && grounded)
                 Jump();
         }
 
         private void FixedUpdate()
         {
-            Move();
-            CheckForGround();
+            if (_canMove)
+                Move();
+        }
 
-            if (_grounded)
-                ApplyGroundLinearDrag();
-
-            else
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
             {
-                ApplyAirLinearDrag();
-                FallMultiplier();
+                grounded = true;
+                _canMove = true;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
+            {
+                grounded = false;
+                _canMove = false;
             }
         }
 
         #region Horizontal Movement
         private void Move()
         {
-            _rigidbody2D.AddForce(new Vector2(horizontalDirection, 0f) * _movementAcceleration);
-
-            if (Mathf.Abs(_rigidbody2D.velocity.x) > _maxMoveSpeed)
-                _rigidbody2D.velocity = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x) * _maxMoveSpeed, _rigidbody2D.velocity.y);
-        }
-
-        private void ApplyGroundLinearDrag()
-        {
-            if (Mathf.Abs(horizontalDirection) < 0.4f || changingDirection)
-                _rigidbody2D.drag = _groundLinearDrag;
-
-            else
-                _rigidbody2D.drag = 0f;
+            transform.Translate(Vector3.right * horizontalDirection * _movementSpeed * Time.deltaTime);
         }
 
         private Vector2 GetInput()
@@ -97,30 +99,7 @@ namespace CombatAI.Game.Characters.Player
         #region Jump
         private void Jump()
         {
-            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0f);
-            _rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-        }
-
-        private void ApplyAirLinearDrag()
-        {
-            _rigidbody2D.drag = _airLinearDrag;
-        }
-
-        private void FallMultiplier()
-        {
-            if (_rigidbody2D.velocity.y < 0f)
-                _rigidbody2D.gravityScale = _fallMultiplier;
-
-            else if (_rigidbody2D.velocity.y > 0f && !Input.GetButton("Jump"))
-                _rigidbody2D.gravityScale = _lowJumpFallMultiplier;
-
-            else
-                _rigidbody2D.gravityScale = 1f;
-        }
-
-        private void CheckForGround()
-        {
-            _grounded = Physics2D.Raycast(transform.position * _groundRaycastLength, Vector2.down, _groundRaycastLength, _groundLayer);
+            _rigidbody2D.AddForce(_jumpDirection.normalized * _jumpForce, ForceMode2D.Impulse);
         }
         #endregion
 
@@ -128,7 +107,7 @@ namespace CombatAI.Game.Characters.Player
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawLine(transform.position, transform.position + Vector3.down * _groundRaycastLength);
+            Gizmos.DrawLine(transform.position, transform.position + (Vector3)_jumpDirection);
         }
         #endregion
     }
